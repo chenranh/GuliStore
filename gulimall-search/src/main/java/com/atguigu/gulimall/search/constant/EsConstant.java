77b66673fadb979c8ca8098353477404de780ec4
tiddler <627617510@qq.com>
2020-09-15 11:22:13 +0800

    前端页面修改 点击属性 筛选页面跳转

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/constant/EsConstant.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/constant/EsConstant.java
@@ -2,6 +2,6 @@ package com.atguigu.gulimall.search.constant;
 
 public class EsConstant {
     public static final String PRODUCT_INDEX="gulimallproduct";//sku数据在es中的索引
-    public static final Integer PRODUCT_PAGESIZE=2;//分页
+    public static final Integer PRODUCT_PAGESIZE=16;//分页
 
 }
