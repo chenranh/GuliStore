bcc0f605b7643928941e038d00f68217e6cc215a
yuke <627617510@qq.com>
2020-09-21 11:18:56 +0800

    注册页环境  注意传给前端的数据校验 分布式下session的解决

--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
@@ -5,16 +5,28 @@ import com.atguigu.common.constant.AuthServerConstant;
 import com.atguigu.common.exception.BizCodeEnume;
 import com.atguigu.common.utils.R;
 import com.atguigu.gulimall.auth.feign.ThirdPartFeignService;
+import com.atguigu.gulimall.auth.vo.UserRegistVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 
 /**
@@ -38,6 +50,7 @@ public class LoginController {
 
         // todo 1.接口防刷
 
+
         //防止同一个手机号在60秒内再次发送验证码
         String redisCode = redisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + phone);
         if(StrUtil.isNotBlank(redisCode)){
@@ -48,13 +61,39 @@ public class LoginController {
             }
         }
 
-        //2.验证码的再次校验。存入redis 同时加入当前时间 存入类型 key-phone,value-code sms:code:13006862213--> 45696
+        //存入redis 同时加入当前时间 存入类型 key-phone,value-code sms:code:13006862213--> 45696
         String code = UUID.randomUUID().toString().substring(0, 5)+"_"+System.currentTimeMillis();
         //redis缓存验证码
         redisTemplate.opsForValue().set(AuthServerConstant.SMS_CODE_CACHE_PREFIX+phone,code,10, TimeUnit.MINUTES);
         //远程调用
         thirdPartFeignService.sendCode(phone);
         return R.ok();
+    }
+
+
+    /**
+     *  //todo 重定向携带数据，利用session原理。将数据放在session中，只要跳到下一个页面取出这个数据后，session中的数据就会删除掉
+     *  //todo 分布式下的session问题
+     * @param vo
+     * @param result
+     * @param redirectAttributes 模拟重定向携带数据
+     * @return
+     */
+    @PostMapping("/regist")
+    public String regist(@Valid UserRegistVo vo, BindingResult result, RedirectAttributes redirectAttributes, HttpSession session){
+
+        if (result.hasErrors()){
+            Map<String, String> errors = result.getFieldErrors().stream()
+                    .collect(Collectors.toMap(FieldError::getField, DefaultMessageSourceResolvable::getDefaultMessage));
+
+            redirectAttributes.addFlashAttribute("errors",errors);
+            //校验出错，转发到注册页
+            return "redirect:http://auth.gulimall.com/reg.html";
+        }
+        //真正注册 调用远程服务进行注册
+
+        //注册成功回到首页，回到登录页
+        return "redirect:/login.html";
     }
 
 
