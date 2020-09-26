e2b6181e9c6d4e9d769459cb12748f62751e050b
yuke <627617510@qq.com>
2020-09-26 09:29:35 +0800

    优化购物车 有此商品修改数量 无此商品才添加到购物车

--- gulimall-cart/src/main/java/com/atguigu/gulimallcart/service/impl/CartServiceImpl.java
+++ gulimall-cart/src/main/java/com/atguigu/gulimallcart/service/impl/CartServiceImpl.java
@@ -1,5 +1,6 @@
 package com.atguigu.gulimallcart.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.atguigu.common.utils.R;
@@ -9,7 +10,9 @@ import com.atguigu.gulimallcart.service.CartService;
 import com.atguigu.gulimallcart.vo.CartItem;
 import com.atguigu.gulimallcart.vo.SkuInfoVo;
 import com.atguigu.gulimallcart.vo.UserInfoTo;
+import io.netty.util.internal.StringUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -33,55 +36,67 @@ public class CartServiceImpl implements CartService {
     @Autowired
     ThreadPoolExecutor executor;
 
-    private final String CART_PREFIX="gulimall:cart:";
+    private final String CART_PREFIX = "gulimall:cart:";
 
     @Override
     public CartItem addToCart(Long skuId, Integer num) throws ExecutionException, InterruptedException {
         BoundHashOperations<String, Object, Object> cartOps = getCartOps();
-        //  添加商品到购物车
-        CartItem cartItem = new CartItem();
 
-        //1.远程查询当前要添加的商品的信息
-        CompletableFuture<Void> getSkuInfoTask = CompletableFuture.runAsync(() -> {
-            R skuInfo = productFeignService.getSkuInfo(skuId);
-            SkuInfoVo data = skuInfo.getData("skuInfo", new TypeReference<SkuInfoVo>() {
-            });
+        String res = (String) cartOps.get(skuId.toString());
 
-            cartItem.setCount(num);
-            cartItem.setCheck(true);
-            cartItem.setImage(data.getSkuDefaultImg());
-            cartItem.setPrice(data.getPrice());
-            cartItem.setTitle(data.getSkuTitle());
-            cartItem.setSkuId(skuId);
-        }, executor);
+        if (StrUtil.isEmpty(res)) {
+            //购物车中无此商品 添加新商品到购物车
+            CartItem cartItem = new CartItem();
+            //1.远程查询当前要添加的商品的信息
+            CompletableFuture<Void> getSkuInfoTask = CompletableFuture.runAsync(() -> {
+                R skuInfo = productFeignService.getSkuInfo(skuId);
+                SkuInfoVo data = skuInfo.getData("skuInfo", new TypeReference<SkuInfoVo>() {
+                });
 
-        //2.远程查询sku的组合信息
-        CompletableFuture<Void> getSkuSaleAttrValues = CompletableFuture.runAsync(() -> {
-            List<String> values = productFeignService.getSkuSaleAttrValues(skuId);
-            cartItem.setSkuAttr(values);
-        }, executor);
+                cartItem.setCount(num);
+                cartItem.setCheck(true);
+                cartItem.setImage(data.getSkuDefaultImg());
+                cartItem.setPrice(data.getPrice());
+                cartItem.setTitle(data.getSkuTitle());
+                cartItem.setSkuId(skuId);
+            }, executor);
 
-        //等异步线程都执行完再给redis中放数据 get()是阻塞式等待
-        CompletableFuture.allOf(getSkuSaleAttrValues,getSkuInfoTask).get();
+            //2.远程查询sku的组合信息
+            CompletableFuture<Void> getSkuSaleAttrValues = CompletableFuture.runAsync(() -> {
+                List<String> values = productFeignService.getSkuSaleAttrValues(skuId);
+                cartItem.setSkuAttr(values);
+            }, executor);
 
-        String s = JSON.toJSONString(cartItem);
-        cartOps.put(skuId.toString(),s);
-        return cartItem;
+            //等异步线程都执行完再给redis中放数据 get()是阻塞式等待
+            CompletableFuture.allOf(getSkuSaleAttrValues, getSkuInfoTask).get();
+
+            String s = JSON.toJSONString(cartItem);
+            cartOps.put(skuId.toString(), s);
+            return cartItem;
+        } else {
+            //购物车有此商品，修改数量
+            CartItem cartItem = JSON.parseObject(res, CartItem.class);
+            cartItem.setCount(cartItem.getCount() + num);
+            cartOps.put(skuId, JSON.toJSONString(cartItem));
+            return cartItem;
+        }
+
     }
 
     /**
      * 获取到我们要操作的购物车
+     *
      * @return
      */
     private BoundHashOperations<String, Object, Object> getCartOps() {
         //1.得到用户信息
         UserInfoTo userInfoTo = CartInterceptor.threadLocal.get();
         //根据用户是否登录选择存入redis不同的key
-        String cartKey="";
-        if (userInfoTo.getUserId()!=null){
-            cartKey=CART_PREFIX+userInfoTo.getUserId();
-        }else {
-            cartKey=CART_PREFIX+userInfoTo.getUserKey();
+        String cartKey = "";
+        if (userInfoTo.getUserId() != null) {
+            cartKey = CART_PREFIX + userInfoTo.getUserId();
+        } else {
+            cartKey = CART_PREFIX + userInfoTo.getUserKey();
         }
         BoundHashOperations<String, Object, Object> cartOps = redisTemplate.boundHashOps(cartKey);
         //绑定购物车的key
