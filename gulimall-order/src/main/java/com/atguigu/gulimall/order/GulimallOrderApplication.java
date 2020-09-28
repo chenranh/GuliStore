72191b71b700874d84b2b74fe2e1ea61534517c5
tiddler <627617510@qq.com>
2020-09-28 08:35:00 +0800

    rabbitmq测试创建交换机，队列和交换机队列的绑定关系

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -1,9 +1,20 @@
 package com.atguigu.gulimall.order;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 使用rqbbitmq
+ * 1.引入amqp依赖 rabbitautoconfiguration自动生效
+ * 2.自动配置
+ * 3.yml文件配置以spring.rabbitmq开头的配置
+ * 4.@enableRabbit
+ *
+ */
+
+@EnableRabbit //开启mq
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GulimallOrderApplication {
--- gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,13 +1,75 @@
 package com.atguigu.gulimall.order;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.lang.Nullable;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Map;
+
+@Slf4j
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class GulimallOrderApplicationTests {
 
+    @Autowired
+    AmqpAdmin amqpAdmin;
+
+
+    /**
+     * 1.如何创建exchange queue binding
+     * 使用 amqpAdmin进行创建
+     * 2.如何收发消息
+     */
+
+//========================================================创建部分==========================================================
+    //创建交换机
     @Test
-    void contextLoads() {
+    void createExchange() {
+//        DirectExchange(String name, boolean durable, boolean autoDelete, Map<String, Object> arguments)
+
+        DirectExchange directExchange = new DirectExchange("hello-java-exchange", true, false);
+        amqpAdmin.declareExchange(directExchange);
+        log.info("Exchange{}创建成功", "hello-java-exchange");
     }
+
+    //创建队列
+    @Test
+    void createQueue() {
+//        Queue(String name, boolean durable, boolean exclusive, boolean autoDelete, @Nullable Map<String, Object> arguments)
+//        exclusive参数 表示只能有一个能连接到队列
+        Queue queue = new Queue("hello-java-queue", true, false, false);
+        amqpAdmin.declareQueue(queue);
+        log.info("Queue{}创建成功", "hello-java-queue");
+    }
+
+
+    //创建绑定关系
+    @Test
+    void createBinding() {
+//        Binding(String destination, 目的地 队列
+//        Binding.DestinationType destinationType, 目的地类型
+//        String exchange,交换机
+//        String routingKey,路由键
+//        @Nullable Map<String, Object> arguments) 自定义参数
+
+
+        //将交换机和目的地进行绑定 使用routingKey作为路由键
+        Binding binding = new Binding("hello-java-queue",
+                Binding.DestinationType.QUEUE,
+                "hello-java-exchange",
+                "hello-java", null);
+        amqpAdmin.declareBinding(binding);
+        log.info("Binding{}创建成功", "hello-java-binding");
+    }
+    //================================================收发消息=================================================
+
 
 }
