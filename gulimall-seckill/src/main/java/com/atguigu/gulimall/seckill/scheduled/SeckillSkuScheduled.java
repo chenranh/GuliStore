6619327db80e30aef45b73a5c348576c1a3df020
yuke <627617510@qq.com>
2020-10-16 18:19:07 +0800

    重要 秒杀服务添加活动的幂等性。 分布式系统使用分布式锁，保证只能有一台服务器上架秒杀活动，redis保存之前对key进行判断，保证活动不被重复上架

--- gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/scheduled/SeckillSkuScheduled.java
+++ gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/scheduled/SeckillSkuScheduled.java
@@ -2,16 +2,20 @@ package com.atguigu.gulimall.seckill.scheduled;
 
 import com.atguigu.gulimall.seckill.service.SeckillService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 秒杀商品的定时上架
- *    每天晚上3点 上架最近三天需要秒杀的商品
- *    当天00:00:00-23:59:59
- *    明天00:00:00-23:59:59
- *    后天00:00:00-23:59:59
+ * 每天晚上3点 上架最近三天需要秒杀的商品
+ * 当天00:00:00-23:59:59
+ * 明天00:00:00-23:59:59
+ * 后天00:00:00-23:59:59
  *
  * @Author yuke
  * @Date: 2020-10-16 14:47
@@ -25,11 +29,28 @@ public class SeckillSkuScheduled {
     @Autowired
     SeckillService seckillService;
 
+    @Autowired
+    RedissonClient redissonClient;
+
+    private final String UPLOAD_LOCK = "seckill:upload:lock";
+
     //// TODO: 2020-10-16 上架幂等性 上架了就不能再上架了
     @Scheduled(cron = "0 0 3 * * ?")
-    public void uploadSeckillSkuLatest3Days(){
+    public void uploadSeckillSkuLatest3Days() {
+
         //1.重复上架无需处理
-        seckillService.uploadSeckillSkuLatest3Day();
+
+        //分布式锁  解决分布式服务器共同上架的问题
+        //锁的业务执行完成，状态已经更新完成，释放锁以后其它人获取到的是最新的状态
+        RLock lock = redissonClient.getLock(UPLOAD_LOCK);
+
+        lock.lock(10, TimeUnit.SECONDS);
+        try {
+            seckillService.uploadSeckillSkuLatest3Day();
+        } finally {
+            lock.unlock();
+        }
+
     }
 
 }
--- gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/service/impl/SeckillServiceImpl.java
+++ gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/service/impl/SeckillServiceImpl.java
@@ -71,9 +71,13 @@ public class SeckillServiceImpl implements SeckillService {
             Long startTime = session.getStartTime().getTime();
             Long endTime = session.getEndTime().getTime();
             String key = SESSION_CACHE_PREFIX + startTime + "_" + endTime;
-            List<String> collect = session.getRelationSkus().stream().map(item -> item.getSkuId().toString()).collect(Collectors.toList());
-            //缓存活动信息
-            stringRedisTemplate.opsForList().leftPushAll(key, collect);
+            //缓存活动信息  如果key已经存在就不再保存了，解决活动重复保存的问题
+            Boolean hasKey = stringRedisTemplate.hasKey(key);
+            if (!hasKey) {
+                List<String> collect = session.getRelationSkus().stream().map(item -> item.getPromotionId()+"_"+item.getSkuId()).collect(Collectors.toList());
+                stringRedisTemplate.opsForList().leftPushAll(key, collect);
+            }
+
         });
 
     }
@@ -84,37 +88,40 @@ public class SeckillServiceImpl implements SeckillService {
             BoundHashOperations<String, Object, Object> ops = stringRedisTemplate.boundHashOps(SKUKILL_CACHE_PREFIX);
 
             session.getRelationSkus().stream().forEach(seckillSkuVo -> {
-                //缓存商品
-                SeckillSkuRedisTo redisTo = new SeckillSkuRedisTo();
 
-                //1.sku的基本数据
 
-                R skuInfo = productFeignService.skuInfo(seckillSkuVo.getSkuId());
-                if (skuInfo.getCode() == 0) {
-                    SkuInfoVo info = skuInfo.getData("skuInfo", new TypeReference<SkuInfoVo>() {
-                    });
-                    redisTo.setSkuInfoVo(info);
+                //如果redis中已经存在就不在存放，解决重复上架秒杀活动问题
+                if (!ops.hasKey(seckillSkuVo.getPromotionId()+"_"+seckillSkuVo.getSkuId())) {
+                    //缓存商品
+                    SeckillSkuRedisTo redisTo = new SeckillSkuRedisTo();
+
+                    //1.sku的基本数据
+                    R skuInfo = productFeignService.skuInfo(seckillSkuVo.getSkuId());
+                    if (skuInfo.getCode() == 0) {
+                        SkuInfoVo info = skuInfo.getData("skuInfo", new TypeReference<SkuInfoVo>() {
+                        });
+                        redisTo.setSkuInfoVo(info);
+                    }
+                    //2.sku的秒杀信息
+                    BeanUtil.copyProperties(seckillSkuVo, redisTo);
+
+                    //3.设置上当前商品的秒杀时间信息
+                    redisTo.setStartTime(session.getStartTime().getTime());
+                    redisTo.setEndTime(session.getEndTime().getTime());
+
+                    //4.商品秒杀的随机码  公平秒杀，减库存时也需要这个随机码
+                    String token = UUID.randomUUID().toString().replace("-", "");
+
+                    redisTo.setRandomCode(token);
+                    String jsonString = JSON.toJSONString(redisTo);
+                    ops.put(seckillSkuVo.getPromotionId()+"_"+seckillSkuVo.getSkuId(), jsonString);
+
+                    //5.使用库存作为引入redisson的信号量  作用是限流！！！！！！！！！！
+                    RSemaphore semaphore = redissonClient.getSemaphore(SKUSTOCK_SEMAPHONE + token);
+                    //商品可以秒杀的数量作为信号量
+                    semaphore.trySetPermits(seckillSkuVo.getSeckillCount());
                 }
 
-                //2.sku的秒杀信息
-                BeanUtil.copyProperties(seckillSkuVo, redisTo);
-
-                //3.设置上当前商品的秒杀时间信息
-                redisTo.setStartTime(session.getStartTime().getTime());
-                redisTo.setEndTime(session.getEndTime().getTime());
-
-                //4.商品秒杀的随机码  公平秒杀，减库存时也需要这个随机码
-                String token = UUID.randomUUID().toString().replace("-", "");
-                redisTo.setRandomCode(token);
-
-                //5.使用库存作为引入分布式的信号量  作用是限流！！！！！！！！！！
-                RSemaphore semaphore = redissonClient.getSemaphore(SKUSTOCK_SEMAPHONE + token);
-                //商品可以秒杀的数量作为信号量
-                semaphore.trySetPermits(seckillSkuVo.getSeckillCount());
-
-
-                String jsonString = JSON.toJSONString(redisTo);
-                ops.put(seckillSkuVo.getSkuId().toString(), jsonString);
             });
         });
     }
